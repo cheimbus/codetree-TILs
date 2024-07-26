cee352647ab73a6d988e1b99d937f90cd879597b
Siu Hwang <siuh0403@gmail.com>
2024-07-26 13:57:46 +0900

    Title: 기수 정렬 구현, Time: 497ms, Memory: 34MB, Status: Passed - CodeTree

--- 240726/기수 정렬 구현/implement-radix-sort.java
+++ 240726/기수 정렬 구현/implement-radix-sort.java
@@ -1,55 +1,51 @@
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
+        
         int n = Integer.parseInt(bf.readLine());
 
         String[] sArr = bf.readLine().split(" ");
-
         int[] iArr = new int[sArr.length];
         for(int i = 0; i < sArr.length; i++) {
             iArr[i] = Integer.parseInt(sArr[i]);
         }
 
         int k = getMaxLeng(iArr);
-        
-        radixSort(iArr, k);
 
+        radixSort(iArr, k);
+        
         StringBuffer sb = new StringBuffer();
         for(int i : iArr) {
             sb.append(i).append(" ");
         }
-
-        sb.setLength(sb.length()-1);
+        sb.setLength(sb.length() -1);
 
         System.out.println(sb);
     }
 
     public static void radixSort(int[] arr, int k) {
         for(int pos = 0; pos < k; pos ++) {
-
             List<List<Integer>> newArr = new ArrayList<>(10);
-            for(int i = 0; i < 10; i ++) {
+            for(int i = 0; i < 10; i++) {
                 newArr.add(new ArrayList<>());
             }
 
-        for(int num : arr) {
-            int digit = getDigit(num, pos);
-            newArr.get(digit).add(num);
-        }
-
-        int idx = 0;
-        for(List<Integer> bucket : newArr) {
-            for(int num : bucket) {
-                arr[idx++] = num;
+            for(int num : arr) {
+                int digit = getDigit(num, pos);
+                newArr.get(digit).add(num);
             }
-        }
 
+            int idx = 0;
+            for(List<Integer> bucket : newArr) {
+                for(int num : bucket) {
+                    arr[idx++] = num;
+                }
+            }
         }
     }
 
@@ -64,5 +60,4 @@ public class Main {
         }
         return Integer.toString(max).length();
     }
-
 }
