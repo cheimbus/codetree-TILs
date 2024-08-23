dd6e5dcf98c296794c9e8af9200cfb5f36699282
Siu Hwang <siuh0403@gmail.com>
2024-08-23 14:45:41 +0900

    Title: 최고의 33위치, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

--- 240823/최고의 33위치/best-place-of-33.java
+++ 240823/최고의 33위치/best-place-of-33.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 public class Main {
     public static void main(String[] args) throws IOException {
         
@@ -16,19 +17,23 @@ public class Main {
             list.add(inputList);
         }
 
-        int result = 0;
+        List<Integer> resultList = new ArrayList<>();
 
-        for(int i = 0; i <= num -3; i++) {
-            for(int j = 0; j <= num -3; j++) {
-                for(int k = i; k < i + 3; k++) {
-                    for(int l = j; l < j + 3; l++) {
-                        if(list.get(k)[l].equals("1")) {
-                            result ++;
+         for (int i = 0; i <= num - 3; i++) {
+            for (int j = 0; j <= num - 3; j++) {
+                int cnt = 0;
+                for (int k = i; k < i + 3; k++) {
+                    for (int l = j; l < j + 3; l++) {
+                        if (list.get(k)[l].equals("1")) {
+                            cnt++;
                         }
                     }
                 }
+                resultList.add(cnt);
             }
         }
+
+        int result = Collections.max(resultList);
 
         System.out.println(result);
     }
