e8656bbce21524c3371eb0962cb84ce6f2ecbd67
Siu Hwang <siuh0403@gmail.com>
2024-08-27 16:22:41 +0900

    Title: 아름다운 수, Time: 82ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240827/아름다운 수/beautiful-number.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+public class Main {
+
+    public static int n;
+    public static ArrayList<Integer> selectedNum = new ArrayList<>();
+    public static int answer;
+
+    public static boolean beautifulNum() {
+        for(int i = 0; i < n; i += selectedNum.get(i)) {
+            int target = selectedNum.get(i);
+            int cnt = selectedNum.get(i);
+            for(int j = i; j < n; j++) {
+                if(target == selectedNum.get(j)) {
+                    cnt --;
+                    if(cnt == 0) {
+                        continue;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void permutations(int num) {
+        
+        if(num == n) {
+            if(beautifulNum()) {
+                answer++;
+            }
+            return;
+        }
+
+        for(int i = 1; i <= 4; i++) {
+            selectedNum.add(i);
+            permutations(num +1);
+            selectedNum.remove(selectedNum.size() -1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        permutations(0);
+
+        System.out.print(answer);
+    }
+}
