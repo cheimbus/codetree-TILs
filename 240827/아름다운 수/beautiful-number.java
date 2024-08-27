2969424be7d363c4e0acfb041bd38468ce3af2dc
Siu Hwang <siuh0403@gmail.com>
2024-08-27 22:40:23 +0900

    Title: 아름다운 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240827/아름다운 수/beautiful-number.java
+++ 240827/아름다운 수/beautiful-number.java
@@ -2,41 +2,24 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 public class Main {
-
     public static int n;
     public static ArrayList<Integer> selectedNum = new ArrayList<>();
     public static int answer;
-
-    public static boolean beautifulNum() {
-    for(int i = 0; i < n; i += selectedNum.get(i)) {
-        int target = selectedNum.get(i);
-        int cnt = 0;
-
-        for(int j = i; j < n; j++) {
-            if(selectedNum.get(j) == target) {
-                cnt++;
-                if(cnt == target) continue;
+    public static boolean beatifulNum() {
+        for(int i = 0; i < n; i += selectedNum.get(i)) {
+            for(int j = i; j < i + selectedNum.get(i); j++) {
+                System.out.println(selectedNum.get(j));
             }
         }
-
-        if(cnt != target) {
-            return false;
-        }
+        return true;
     }
-    return true;
-}
 
     public static void permutations(int num) {
-        
         if(num == n) {
-            if(beautifulNum()) {
-                answer++;
-            }
+            beatifulNum();
             return;
         }
-
         for(int i = 1; i <= 4; i++) {
             selectedNum.add(i);
             permutations(num +1);
@@ -48,8 +31,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
-        permutations(0);
 
-        System.out.print(answer);
+        permutations(0);
+        
+        System.out.println(answer);
     }
 }
