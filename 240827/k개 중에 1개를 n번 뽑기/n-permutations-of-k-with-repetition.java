9b4ff63d7f6be186a0525886ca9425d66e8000aa
Siu Hwang <siuh0403@gmail.com>
2024-08-27 10:35:18 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240827/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+public class Main {
+
+    public static int k, n;
+    public static ArrayList<Integer> selectedNum = new ArrayList<>();
+
+    public static void permutation() {
+        for(int i : selectedNum) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void findPermutations(int cnt) {
+        if(cnt == n) {
+            permutation();
+            return;
+        }
+
+        for(int i = 1; i <= k; i ++) {
+            selectedNum.add(i);
+            findPermutations(cnt +1);
+            selectedNum.remove(selectedNum.size() -1);
+        }
+    }
+
+    public static void main(String[] args) throws IOExecption {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        k = Integer.parseInt(stk.nextToken());
+        n = Integer.parseInt(stk.nextToken());
+
+        findPermutations(0);
+    }
+}
