8f14a6861c86385c2c0e6bfbd1e0edd112c015aa
Siu Hwang <siuh0403@gmail.com>
2024-08-27 10:37:15 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 1335ms, Memory: 22MB, Status: Passed - CodeTree

--- 240827/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
+++ 240827/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -28,7 +28,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws IOExecption {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
 
