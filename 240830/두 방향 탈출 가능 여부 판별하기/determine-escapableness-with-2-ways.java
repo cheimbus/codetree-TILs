76eb4218361d8053190bcd3f7c7c9dfdb68ed5bf
Siu Hwang <siuh0403@gmail.com>
2024-08-30 15:17:12 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240830/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
+++ 240830/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -9,7 +9,7 @@ public class Main {
     public static int[][] visited;
     public static int dx[] = new int[]{1, 0};
     public static int dy[] = new int[]{0, 1};
-    public static ArrayList<Integer> answer = new ArrayList<>();
+    public static int[][] answer;
 
     public static boolean canGo(int x, int y) {
         if(x < 0 || x >= N || y < 0 || y >= M) {
@@ -29,8 +29,7 @@ public class Main {
             setY = y + dy[i];
             if(canGo(setX, setY)) {
                 visited[setX][setY] = 1;
-                answer.add(setX);
-                answer.add(setY);
+                answer[setX][setY] = 1;
                 DFS(setX, setY);
             }
         }
@@ -45,6 +44,7 @@ public class Main {
 
         grid = new int[N][M];
         visited = new int[N][M];
+        answer = new int[N][M];
         
         for(int i = 0; i < N; i++) {
             stk = new StringTokenizer(br.readLine());
@@ -55,9 +55,10 @@ public class Main {
         }
 
         visited[0][0] = 1;
+        answer[0][0] = 1;
         DFS(0, 0);
         
-        if(answer.get(answer.size() -2) == N-1 && answer.get(answer.size()-1) == M-2) System.out.print(1);
-        else System.out.print(0);
+        if(answer[N-1][M-1] != 1) System.out.print(0);
+        else System.out.print(1);
     }
 }
