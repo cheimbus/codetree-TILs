40e80def862359828db372556aa4e57ae930434d
Siu Hwang <siuh0403@gmail.com>
2024-09-01 16:06:55 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240901/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
+++ 240901/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -1,30 +1,25 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.util.Queue;
+import java.util.LinkedList;
 public class Main {
-    public static int N,M;
-    public static int[][] grid;
-    public static boolean[][] visited;
-    public static Queue<Pair> q = new LinkedList<>();
-    public static int[] dx = new int[]{1, 0, -1, 0};
-    public static int[] dy = new int[]{0, 1, 0, -1};
 
-     static class Pair {
-        int x, y;
+    static class Pair {
+        int x,y;
         public Pair(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
 
-    public static void push(int x, int y) {
-        visited[x][y] = true;
-        q.add(new Pair(x, y));
-    }
+    public static int N,M;
+    public static int[][] grid;
+    public static Queue<Pair> q = new LinkedList<>();
+    public static boolean[][] visited;
+    public static int[] dx = new int[]{1, 0, -1, 0};
+    public static int[] dy = new int[]{0, 1, 0, -1};
 
     public static boolean canGo(int x, int y) {
         if(x < 0 || x >= N || y < 0 || y >= M) {
@@ -36,11 +31,16 @@ public class Main {
         return true;
     }
 
+    public static void push(int x, int y) {
+        visited[x][y] = true;
+        q.add(new Pair(x, y));
+    }
+
     public static void BFS() {
         while(!q.isEmpty()) {
-            Pair newVertax = q.poll();
-            int x = newVertax.x;
-            int y = newVertax.y;
+            Pair curr = q.poll();
+            int x = curr.x;
+            int y = curr.y;
             for(int i = 0; i < dx.length; i++) {
                 int newX = x + dx[i];
                 int newY = y + dy[i];
@@ -54,24 +54,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
-        
+
         N = Integer.parseInt(stk.nextToken());
         M = Integer.parseInt(stk.nextToken());
 
-        visited = new boolean[N][M];
         grid = new int[N][M];
 
         for(int i = 0; i < N; i++) {
             stk = new StringTokenizer(br.readLine());
             for(int j = 0; j < M; j++) {
-                int nextV = Integer.parseInt(stk.nextToken());
-                grid[i][j] = nextV;
+                grid[i][j] = Integer.parseInt(stk.nextToken());
             }
         }
 
-        push(0, 0);
-        BFS();   
+        visited = new boolean[N][M];
 
+        push(0, 0);
+        BFS();
+        
         int answer = visited[N-1][M-1] ? 1 : 0;
         System.out.print(answer);
     }
