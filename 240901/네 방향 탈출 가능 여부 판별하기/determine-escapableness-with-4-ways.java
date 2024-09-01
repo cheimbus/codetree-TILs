1612c0b2bd501fd3c3e0f9711a2edb0a39002147
Siu Hwang <siuh0403@gmail.com>
2024-09-01 14:27:01 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240901/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,79 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.Queue;
+public class Main {
+    public static int N,M;
+    public static int[][] grid;
+    public static boolean[][] visited;
+    public static Queue<Pair> q = new LinkedList<>();
+    public static int order = 1;
+    public static int[] dx = new int[2];
+    public static int[] dy = new int[2];
+
+     static class Pair {
+        int x, y;
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void push(int x, int y) {
+        visited[x][y] = true;
+        q.add(new Pair(x, y));
+    }
+
+    public static boolean isRange(int x, int y) {
+        if(x < 0 || x >= N || y < 0 || y >= M) return false;
+        else return true;
+    }
+
+    public static boolean canGo(int x, int y) {
+        if(isRange(x, y) || visited[x][y] || grid[x][y] == 0) return false;
+        else return true;
+    }
+
+    public static void BFS() {
+        while(!q.isEmpty()) {
+            Pair newVertax = q.poll();
+            int x = newVertax.x;
+            int y = newVertax.y;
+            for(int i = 0; i < dx.length; i++) {
+                int newX = x + dx[i];
+                int newY = y + dx[i];
+                if(canGo(newX, newY)) {
+                    push(newX, newY);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(stk.nextToken());
+        M = Integer.parseInt(stk.nextToken());
+
+        visited = new boolean[N][M];
+        grid = new int[N][N];
+
+        for(int i = 0; i < N; i++) {
+            stk = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++) {
+                int nextV = Integer.parseInt(stk.nextToken());
+                grid[i][j] = nextV;
+            }
+        }
+
+        push(0, 0);
+        BFS();
+
+        if(visited[N-1][M-1] != false) System.out.print(0);
+        else System.out.print(1);
+    }
+}
