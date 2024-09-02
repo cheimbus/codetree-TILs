37084f2ea2f45f60d92225a55812a0293e0638cc
Siu Hwang <siuh0403@gmail.com>
2024-09-02 15:53:04 +0900

    Title: 갈 수 있는 곳들, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240902/갈 수 있는 곳들/places-can-go.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+public class Main {
+
+    static class Pair {
+        int col, row;
+        public Pair(int col, int row) {
+            this.col = col;
+            this.row = row;
+        }
+    }
+
+    public static int n, k;
+    public static int[][] grid;
+    public static int cnt = 0;
+    public static int[] dx = new int[]{1, 0, -1, 0};
+    public static int[] dy = new int[]{0, 1, 0, -1};
+    public static Queue<Pair> q = new LinkedList<>();
+
+    public static boolean canGo(int col, int row) {
+        if(col < 1 || col > n || row < 1 || row > n) {
+            return false;
+        }
+        if(grid[col][row] == 1) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void push(int col, int row) {
+        q.add(new Pair(col, row));
+    }
+
+    public static void BFS(int col, int row) {
+        for(int i = 0; i < dx.length; i++) {
+            while(!q.isEmpty()) {
+                Pair matrix = q.poll();
+                int newCol = matrix.col + dx[i];
+                int newRow = matrix.row + dy[i];
+                if(canGo(newCol, newRow)) {
+                    push(newCol, newRow);
+                    cnt++;
+                }
+            }
+            q.add(new Pair(col, row));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(stk.nextToken());
+        k = Integer.parseInt(stk.nextToken());
+
+        grid = new int[n+1][n+1];
+        visited = new boolean[n+1][n+1];
+
+        for(int i = 1; i <= n; i++) {
+            stk = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= n; j++) {
+                grid[i][j] = Integer.parseInt(stk.nextToken());
+            }
+        }
+
+        for(int i = 0; i < k; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int col = Integer.parseInt(stk.nextToken());
+            int row = Integer.parseInt(stk.nextToken());
+            push(col, row);
+            BFS(col, row);
+        }
+        
+        System.out.print(cnt);
+    }
+}
