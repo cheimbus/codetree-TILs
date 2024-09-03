917ffdaa6d749e2605f8d33fa140c61f8c935ae4
Siu Hwang <siuh0403@gmail.com>
2024-09-03 16:45:04 +0900

    Title: 피보나치 수, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

--- 240903/피보나치 수/fibonacci-number.java
+++ 240903/피보나치 수/fibonacci-number.java
@@ -12,7 +12,7 @@ public class Main {
         dp[0] = 1;
         dp[1] = 1;
 
-        for(int i = 3; i <= n; i++) {
+        for(int i = 2; i < n; i++) {
             dp[i] = dp[i - 2] + dp[i - 1];
         }
 
