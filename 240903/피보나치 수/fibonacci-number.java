bca9f69a2396a9a4a465c4cf475322ab48525a49
Siu Hwang <siuh0403@gmail.com>
2024-09-03 16:29:27 +0900

    Title: 피보나치 수, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240903/피보나치 수/fibonacci-number.java
+++ 240903/피보나치 수/fibonacci-number.java
@@ -1,36 +1,21 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 public class Main {
-    public static int[] dp;
-
-    public static int fibo(int num) {
-        if(dp[num] != -1) {
-            return dp[num];
-        }
-        if(num <= 1) {
-            return dp[num];
-        }
-        else {
-            dp[num] = fibo(num - 2) + fibo(num - 1);
-            return dp[num];
-        }
-
-    }
-
+    public static int[] dp = new int[45];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
 
-        dp = new int[45];
-
-        for(int i = 0; i < 45; i++) {
-            dp[i] = -1;
-        }
         dp[0] = 1;
         dp[1] = 1;
-        int answer = fibo(n - 1);
 
-        System.out.print(answer);
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        System.out.print(dp[n - 1]);
     }
 }
