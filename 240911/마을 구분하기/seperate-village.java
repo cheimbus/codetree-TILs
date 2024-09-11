efb807bdbf0fd62f570358243468bf853a7d7d2e
Siu Hwang <siuh0403@gmail.com>
2024-09-11 23:08:16 +0900

    Title: 마을 구분하기, Time: 116ms, Memory: 9MB, Status: Passed - CodeTree

--- 240911/마을 구분하기/seperate-village.java
+++ 240911/마을 구분하기/seperate-village.java
@@ -7,12 +7,12 @@ import java.util.Collections;
 public class Main {
     public static int n;
     public static ArrayList<Integer>[] graph;
-    public static boolean[][] visited;
-    public static ArrayList<Integer> villages;
     public static int pCnt;
+    public static ArrayList<Integer> villages;
+    public static final int MAX_LEN = 4;
     public static int[] dx = new int[]{1, 0, -1, 0};
     public static int[] dy = new int[]{0, 1, 0, -1};
-    public static final int MX_LEN = 4;
+    public static boolean[][] visited;
 
     public static boolean inRange(int x, int y) {
         return x >= 0 && y >= 0 && x < n && y < n;
@@ -28,13 +28,13 @@ public class Main {
         return true;
     }
 
-    public static void DFS(int vertex1, int vertex2) {
-        for(int i = 0; i < MX_LEN; i++) {
-            int nx = vertex1 + dx[i];
-            int ny = vertex2 + dy[i];
+    public static void DFS(int x, int y) {
+        for(int i = 0; i < MAX_LEN; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
             if(canGo(nx, ny)) {
-                visited[nx][ny] = true;
                 pCnt++;
+                visited[nx][ny] = true;
                 DFS(nx, ny);
             }
         }
@@ -43,11 +43,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
-        
+
         n = Integer.parseInt(stk.nextToken());
         graph = new ArrayList[n];
-        visited = new boolean[n][n];
         villages = new ArrayList<>();
+        visited = new boolean[n][n];
 
         for(int i = 0; i < n; i++) {
             graph[i] = new ArrayList<>();
@@ -60,10 +60,10 @@ public class Main {
                 graph[i].add(num);
             }
         }
-        
+
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                if(canGo(i ,j)) {
+                if(canGo(i, j)) {
                     visited[i][j] = true;
                     pCnt = 1;
                     DFS(i, j);
@@ -71,14 +71,11 @@ public class Main {
                 }
             }
         }
-
         Collections.sort(villages);
         StringBuilder sb = new StringBuilder();
-        sb.append(villages.size());
-        sb.append("\n");
+        sb.append(villages.size()).append("\n");
         for(int i = 0; i < villages.size(); i++) {
-            sb.append(villages.get(i));
-            sb.append("\n");
+            sb.append(villages.get(i)).append("\n");
         }
         System.out.print(sb);
     }
