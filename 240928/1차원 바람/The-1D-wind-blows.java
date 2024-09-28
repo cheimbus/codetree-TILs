c7b7fdb732ae7e9b71c239bc5cc42616cc2879d2
Siu Hwang <siuh0403@gmail.com>
2024-09-28 14:48:28 +0900

    Title: 1차원 바람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240928/1차원 바람/The-1D-wind-blows.java
+++ 240928/1차원 바람/The-1D-wind-blows.java
@@ -7,12 +7,13 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static final int MAX_VAL = 100;
-    public static final int LEFT_SHIFT = 1;
-    public static final int RIGHT_SHIFT = 0;
-    public static int n,m;
-    public static int[][] grid = new int[MAX_VAL + 1][MAX_VAL + 1];
+    public static final int RIGHT_SHIFT = 1;
+    public static final int LEFT_SHIFT = 0;
+    public static int n, m, q;
+    public static int[][] grid = new int[MAX_VAL][MAX_VAL];
 
     public static void shift(int row, int dir) {
+        // 오른쪽
         if(dir == RIGHT_SHIFT) {
             int tmp = grid[row][m];
             for(int col = m; col >= 2; col --) {
@@ -20,6 +21,7 @@ public class Main {
             }
             grid[row][1] = tmp;
         }
+        // 왼쪽
         else {
             int tmp = grid[row][1];
             for(int col = 1; col <= m - 1; col ++) {
@@ -41,7 +43,6 @@ public class Main {
     }
 
     public static void simulation(int curRow, int curDir) {
-
         shift(curRow, curDir);
         curDir = flip(curDir);
 
@@ -53,6 +54,7 @@ public class Main {
             }
             else break;
         }
+
         //아래
         for(int row = curRow, dir = curDir; row <= n - 1; row ++) {
             if(hasSameNum(row, row + 1)) {
@@ -70,27 +72,28 @@ public class Main {
 
         n = Integer.parseInt(stk.nextToken());
         m = Integer.parseInt(stk.nextToken());
-        int q = Integer.parseInt(stk.nextToken());
+        q = Integer.parseInt(stk.nextToken());
 
-        for(int row = 1; row <= n; row ++) {
+        for(int i = 1; i <= n; i ++) {
             stk = new StringTokenizer(br.readLine());
-            for(int col = 1; col <= m; col ++) {
-                grid[row][col] = Integer.parseInt(stk.nextToken());
+            for(int j = 1; j <= m; j ++) {
+                int num = Integer.parseInt(stk.nextToken());
+                grid[i][j] = num;
             }
         }
 
         for(int i = 0; i < q; i ++) {
             stk = new StringTokenizer(br.readLine());
             int row = Integer.parseInt(stk.nextToken());
-            char sd = stk.nextToken().charAt(0);
-            int d = sd == 'L' ? RIGHT_SHIFT : LEFT_SHIFT;
-            simulation(row, d);
+            char cd = stk.nextToken().charAt(0);
+            int dir = cd == 'L' ? RIGHT_SHIFT : LEFT_SHIFT;
+            simulation(row, dir);
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int row = 1; row <= n; row ++) {
-            for(int col = 1; col <= m; col ++) {
-                sb.append(grid[row][col]).append(" ");
+        for(int i = 1; i <= n; i ++) {
+            for(int j = 1; j <= m; j ++) {
+                sb.append(grid[i][j]).append(" ");
             }
             sb.append("\n");
         }
