67cd09ea0d46b921fd60d7bd166dd39d4534f309
Siu Hwang <siuh0403@gmail.com>
2024-09-29 14:41:50 +0900

    Title: 2차원 바람, Time: 190ms, Memory: 10MB, Status: Passed - CodeTree

--- 240929/2차원 바람/The-2D-wind-blows.java
+++ 240929/2차원 바람/The-2D-wind-blows.java
@@ -1,26 +1,27 @@
 import java.io.IOException;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
     public static final int MAX_VAL = 100;
-    public static int[] dx = new int[]{0, 1, 0, -1, 0};
-    public static int[] dy = new int[]{0, 0, 1, 0, -1};
     public static final int D_LEN = 5;
     public static int n, m, q;
     public static int[][] grid = new int[MAX_VAL + 1][MAX_VAL + 1];
     public static int[][] avg = new int[MAX_VAL + 1][MAX_VAL + 1];
+    public static int[] dx = new int[]{0, 1, 0, -1, 0};
+    public static int[] dy = new int[]{0, 0, 1, 0, -1};
 
     public static boolean canGo(int row, int col) {
         return row >= 1 && col >= 1 && n >= row && m >= col;
     }
 
-    public static int getAvg(int row, int col) {
+    public static int sumAvg(int row, int col) {
         int sum = 0;
         int cnt = 0;
+
         for(int i = 0; i < D_LEN; i ++) {
             int nx = row + dx[i];
             int ny = col + dy[i];
@@ -29,13 +30,14 @@ public class Main {
                 cnt ++;
             }
         }
+
         return sum / cnt;
     }
 
-    public static void sumAvg(int row1, int col1, int row2, int col2) {
+    public static void setAvg(int row1, int col1, int row2, int col2) {
         for(int row = row1; row <= row2; row ++) {
             for(int col = col1; col <= col2; col ++) {
-                avg[row][col] = getAvg(row, col);
+                avg[row][col] = sumAvg(row, col);
             }
         }
 
@@ -49,23 +51,22 @@ public class Main {
     public static void shift(int row1, int col1, int row2, int col2) {
 
         int tmp = grid[row1][col1];
-
-        // 위
+        // up
         for(int row = row1; row <= row2 - 1; row ++) {
             grid[row][col1] = grid[row + 1][col1];
         }
 
-        // 왼
+        // left
         for(int col = col1; col <= col2 - 1; col ++) {
             grid[row2][col] = grid[row2][col + 1];
         }
 
-        // 아래
+        // down
         for(int row = row2; row >= row1 + 1; row --) {
             grid[row][col2] = grid[row - 1][col2];
         }
 
-        // 오
+        // right
         for(int col = col2; col >= col1 + 1; col --) {
             grid[row1][col] = grid[row1][col - 1];
         }
@@ -75,7 +76,7 @@ public class Main {
 
     public static void simulation(int row1, int col1, int row2, int col2) {
         shift(row1, col1, row2, col2);
-        sumAvg(row1, col1, row2, col2);
+        setAvg(row1, col1, row2, col2);
     }
 
     public static void main(String[] args) throws IOException {
@@ -87,11 +88,11 @@ public class Main {
         m = Integer.parseInt(stk.nextToken());
         q = Integer.parseInt(stk.nextToken());
 
-        for(int i = 1; i <= n; i ++) {
+        for(int row = 1; row <= n; row ++) {
             stk = new StringTokenizer(br.readLine());
-            for(int j = 1; j <= m; j ++) {
+            for(int col = 1; col <= m; col ++) {
                 int num = Integer.parseInt(stk.nextToken());
-                grid[i][j] = num;
+                grid[row][col] = num;
             }
         }
 
@@ -106,9 +107,9 @@ public class Main {
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= n; i ++) {
-            for(int j = 1; j <= m; j ++) {
-                sb.append(grid[i][j]).append(" ");
+        for(int row = 1; row <= n; row ++) {
+            for(int col = 1; col <= m; col ++) {
+                sb.append(grid[row][col]).append(" ");
             }
             sb.append("\n");
         }
