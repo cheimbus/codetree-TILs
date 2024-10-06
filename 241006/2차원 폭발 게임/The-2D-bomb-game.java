7a96b675fe00ad127c029331b83126e41db987e2
Siu Hwang <siuh0403@gmail.com>
2024-10-06 18:51:37 +0900

    Title: 2차원 폭발 게임, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241006/2차원 폭발 게임/The-2D-bomb-game.java
+++ 241006/2차원 폭발 게임/The-2D-bomb-game.java
@@ -81,16 +81,39 @@ public class Main {
         return cnt;
     }
 
+    public static boolean isBomb() {
+        boolean isBomb = false;
+
+        for(int col = 0; col < n; col ++) {
+            for(int row1 = 0; row1 <= n; row1 ++) {
+                if(grid[row1][col] == 0) continue;
+                int target = grid[row1][col];
+                int mCnt = 1;
+                for(int row2 = row1 + 1; row2 < n; row2 ++) {
+                    if(target == grid[row2][col]) {
+                        mCnt ++;
+                        if(mCnt == m) return true;
+                    }
+                    else break;
+                }
+            }
+        }
+        return isBomb;
+    }
+
     public static int simulation() {
         for(int i = 0; i < k; i ++) {
-            bomb();
-            drop();
+            while(isBomb()) {
+                bomb();
+                drop();
+            }
             tilt();
             drop();
+            while(isBomb()) {
+                bomb();
+                drop();
+            }
         }
-
-        bomb();
-        drop();
 
         int cnt = countBomb();
 
