5800601c6dc08134c6ecafee9e3eccb853411195
Siu Hwang <siuh0403@gmail.com>
2024-10-13 16:48:12 +0900

    Title: 벽이 있는 충돌 실험, Time: 3073ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 241013/벽이 있는 충돌 실험/collision-experiment-with-wall.java
+++ 241013/벽이 있는 충돌 실험/collision-experiment-with-wall.java
@@ -1,114 +1,139 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 class Marble {
-    int x; int y; int dir;
-    public Marble(int x, int y, int dir) {
+    int x, y, z;
+    public Marble(int x, int y, int z) {
         this.x = x;
         this.y = y;
-        this.dir = dir;
+        this.z = z;
     }
 }
 
 public class Main {
-    public static final int LEN = 4;
+    public static final int ASCII_NUM = 128;
+    public static final int MAX_N = 50;
+    public static final int DIR_NUM = 4;
+    
+    // 전역 변수 선언:
     public static int t, n, m;
-    public static ArrayList<Marble> marbleArr = new ArrayList<>();
+    
+    public static int[] mapper = new int[ASCII_NUM];
+    
+    // 후에 구슬이 벽에 부딪혔을 때의 처리를 간단히 하기 위해
+    // dir 기준 0, 3이 대칭 1, 2가 대칭이 되도록 설정합니다.
     public static int[] dx = new int[]{-1, 0, 0, 1};
     public static int[] dy = new int[]{0, 1, -1, 0};
-    public static int[] ASCII_CODE = new int[128];
-    public static StringBuilder sb = new StringBuilder();
 
+    public static ArrayList<Marble> marbles = new ArrayList<>();
+    
+    // 해당 위치가 격자 안에 들어와 있는지 확인합니다.
     public static boolean inRange(int x, int y) {
-        return 1 <= x && 1 <= y && x <= n && y <= n;
+        return 1 <= x && x <= n && 1 <= y && y <= n;
     }
-
-    public static Marble move(Marble marble) {
-        int x = marble.x; int y = marble.y; int dir = marble.dir;
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
-
-        if(inRange(nx, ny)) {
+    
+    // 해당 구슬이 1초 후에 어떤 위치에서 어떤 방향을 보고 있는지를 구해
+    // 그 상태를 반환합니다.
+    public static Marble Move(Marble marble) {
+        // tuple의 경우 다음과 같이 원하는 변수에 값을 뽑아줄 수 있습니다.
+        int x = marble.x;
+        int y = marble.y;
+        int dir = marble.z;
+    
+        // 바로 앞에 벽이 있는지 판단합니다.
+        int nx = x + dx[dir], ny = y + dy[dir];
+        
+        // Case 1 : 벽이 없는 경우에는 그대로 한 칸 전진합니다.
+        if(inRange(nx, ny))
             return new Marble(nx, ny, dir);
-        }
-        else return new Marble(x, y, 3 - dir);
+        // Case 2 : 벽이 있는 경우에는 방향을 반대로 틀어줍니다.
+        // 처음에 dx, dy를 dir 기준 0, 3이 대칭 1, 2가 대칭이 되도록
+        // 설정해놨기 때문에 간단하게 처리가 가능합니다.
+        else
+            return new Marble(x, y, 3 - dir);
     }
-
+    
+    // 구슬을 전부 한 번씩 움직여 봅니다.
     public static void moveAll() {
-        for(int i = 0; i < marbleArr.size(); i ++) {
-            Marble getMarble = marbleArr.get(i);
-            Marble newMarble = move(getMarble);
-            marbleArr.set(i, newMarble);
+        for(int i = 0; i < (int) marbles.size(); i++) {
+            Marble nextMarble = Move(marbles.get(i));
+            marbles.set(i, nextMarble);
         }
     }
-
-    public static boolean duplicateMarbleEx(int idx) {
-        int targetX = marbleArr.get(idx).x;
-        int targetY = marbleArr.get(idx).y;
-        for(int i = 0; i < marbleArr.size(); i ++) {
-            if(idx == i) continue;
-            int nx = marbleArr.get(i).x;
-            int ny = marbleArr.get(i).y;
-            if(targetX == nx && targetY == ny) return true;
+    
+    // 해당 구슬과 충돌이 일어나는 구슬이 있는지 확인합니다.
+    // 자신을 제외한 구슬 중에 위치가 동일한 구슬이 있는지 확인하면 됩니다.
+    public static boolean duplicateMarbleExist(int targetIdx) {
+        int targetX = marbles.get(targetIdx).x;
+        int targetY = marbles.get(targetIdx).y;
+    
+        for(int i = 0; i < (int) marbles.size(); i++) {
+            if(i == targetIdx)
+                continue;
+            
+            int mx = marbles.get(i).x;
+            int my = marbles.get(i).y;
+    
+            if(targetX == mx && targetY == my)
+                return true;
         }
+    
         return false;
     }
-
-    public static void deleteSamePosition() {
-        ArrayList<Marble> newMarbleArr = new ArrayList<>();
-
-        for(int i = 0; i < marbleArr.size(); i ++) {
-            if(!duplicateMarbleEx(i)) {
-                newMarbleArr.add(marbleArr.get(i));
-            }
-        }
-
-        marbleArr = newMarbleArr;
+    
+    // 충돌이 일어나는 구슬은 전부 지워줍니다.
+    public static void removeDuplicateMarbles() {
+        ArrayList<Marble> tempVector = new ArrayList<>();
+    
+        for(int i = 0; i < (int) marbles.size(); i++)
+            if(!duplicateMarbleExist(i))
+                tempVector.add(marbles.get(i));
+        
+        marbles = tempVector;
     }
-
-    public static void simulation() {
+    
+    // 조건에 맞춰 시뮬레이션을 진행합니다.
+    public static void simulate() {
+        // Step1
+        // 구슬을 전부 한 번씩 움직여 봅니다.
         moveAll();
-        deleteSamePosition();
+    
+        // Step2
+        // 움직임 이후에 충돌이 일어나는 구슬들을 골라 목록에서 지워줍니다.
+        removeDuplicateMarbles();
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        mapper['U'] = 0;
+        mapper['R'] = 1;
+        mapper['L'] = 2;
+        mapper['D'] = 3;
 
-        t = Integer.parseInt(br.readLine());
+        // 테스트 케이스 수 입력:
+        t = sc.nextInt();
 
-        ASCII_CODE['U'] = 0;
-        ASCII_CODE['R'] = 1;
-        ASCII_CODE['D'] = 3;
-        ASCII_CODE['L'] = 2;
+        while(t-- > 0) {
+            // 새로운 테스트 케이스가 시작될때마다 기존에 사용하던 값들을 초기화해줍니다.
+            marbles = new ArrayList<>();
 
-        while(t -- > 0) {
-            StringTokenizer stk = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(stk.nextToken());
-            m = Integer.parseInt(stk.nextToken());
-
-            while(m -- > 0) {
-                stk = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(stk.nextToken());
-                int y = Integer.parseInt(stk.nextToken());
-                int dir = ASCII_CODE[stk.nextToken().charAt(0)];
-                marbleArr.add(new Marble(x, y, dir));
+            // 입력:
+            n = sc.nextInt();
+            m = sc.nextInt();
+            for(int i = 1; i <= m; i++) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                char d = sc.next().charAt(0);
+                marbles.add(new Marble(x, y, mapper[d]));
             }
 
-            for(int i = 0; i < n * 2; i ++) {
-                simulation();
-            }
-            sb.append(marbleArr.size()).append("\n");
+            // 2*n번 이후에는 충돌이 절대 일어날 수 없으므로
+            // 시뮬레이션을 총 2*n번 진행합니다.
+            for(int i = 1; i <= 2 * n; i++)
+                simulate();
+            
+            // 출력:
+            System.out.println((int) marbles.size());
         }
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
