be0499920f189d2ca548ff07d06a4cf0ffa632b5
Siu Hwang <siuh0403@gmail.com>
2024-11-19 01:17:58 +0900

    Title: 1차원 윷놀이, Time: 85ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241119/1차원 윷놀이/yutnori-1d.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static final int MAX_N = 12;
+    public static final int MAX_K = 4;
+    public static int n, m, k, cnt;
+    public static int[] turns = new int[MAX_N];
+    public static int[] arr = new int[MAX_K];
+
+    public static void calc() {
+        int c = 0;
+        for(int i = 0; i < k; i ++) {
+            if(arr[i] >= m) c ++;
+        }
+
+        cnt = Math.max(cnt, c);
+    }
+
+    public static void recursion(int currNum) {
+        if(currNum == n) {
+            calc();
+            return;
+        }
+
+        for(int i = 0; i < k; i ++) {
+            if(arr[i] >= m) {
+                continue;
+            }
+
+            arr[i] += turns[currNum];
+            recursion(currNum + 1);
+            arr[i] -= turns[currNum];
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+        k = Integer.parseInt(stk.nextToken());
+
+        stk = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i ++) {
+            turns[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        for(int i = 0; i < k; i ++) {
+            arr[i] = 1;
+        }
+
+        recursion(0);
+
+        bw.write(cnt + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
