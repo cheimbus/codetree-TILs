f966c139bde72097d9192bea58bf0e3da59f8ffc
Siu Hwang <siuh0403@gmail.com>
2024-11-22 18:03:34 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 113ms, Memory: 9MB, Status: Passed - CodeTree

--- 241122/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
+++ 241122/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
@@ -21,49 +21,45 @@ public class Main {
         for(int idx = 0; idx < arr.size(); idx ++) {
             int len = 1;
             while(true) {
-                int str1 = idx, end1 = str1 + len - 1;
-                int str2 = end1 + 1, end2 = str2 + len - 1;
+                int end1 = arr.size() - 1, str1 = end1 - len + 1;
+                int end2 = str1 - 1, str2 = end2 - len + 1;
 
-                if(end2 >= n) break;
+                if(str2 < 0) break;
 
                 if(sameSequence(str1, end1, str2, end2)) {
-                    return true;
+                    return false;
                 }
                 len ++;
             }
         }
-        return false;
+        return true;
     }
 
     public static void recursion(int currNum) {
         if(currNum == n) {
-            if(!possible()) {
-                for(int i = 0; i < arr.size(); i ++) {
-                    sb.append(arr.get(i));
-                }
-                System.out.print(sb.toString());
-                System.exit(0);
+            for(int i = 0; i < arr.size(); i ++) {
+                sb.append(arr.get(i));
             }
+            System.out.print(sb.toString());
+            System.exit(0);
             return;
         }
 
         for(int i = 4; i <= 6; i ++) {
             arr.add(i);
-            recursion(currNum + 1);
+            if(possible()) {
+                recursion(currNum + 1);
+            }
             arr.remove(arr.size() - 1);
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         n = Integer.parseInt(br.readLine());
 
         recursion(0);
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
     }
 }
