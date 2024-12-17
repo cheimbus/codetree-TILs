fc9fa5e8c2963786fc78f688147d492e182f5406
Siu Hwang <siuh0403@gmail.com>
2024-12-17 21:38:55 +0900

    Title: 사각형 채우기, Time: 82ms, Memory: 9MB, Status: Passed - CodeTree

--- 241217/사각형 채우기/rectangle-fill.java
+++ 241217/사각형 채우기/rectangle-fill.java
@@ -5,17 +5,6 @@ public class Main {
     public static int n;
     public static int[] visited;
 
-    public static int recursion(int val) {
-        if(visited[val] != 0) return visited[val];
-
-        if(val <= 2) {
-            if(val == 1) return visited[val] = 1;
-            else return visited[val] = 2;
-        }
-
-        return visited[val] = (recursion(val - 1) + recursion(val - 2)) % 10007;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -23,9 +12,14 @@ public class Main {
         n = Integer.parseInt(br.readLine());
         visited = new int[n + 1];
 
-        int ans = recursion(n);
+        visited[1] = 1;
+        visited[2] = 2;
 
-        bw.write(ans + "");
+        for(int i = 3; i <= n; i ++) {
+            visited[i] = (visited[i - 1] + visited[i - 2]) % 10007;
+        }
+
+        bw.write(visited[n] + "");
         bw.flush();
     }
 }
