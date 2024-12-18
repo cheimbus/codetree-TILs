ec237cfbb4c54ecd2a85396815df5ea2035cf07b
Siu Hwang <siuh0403@gmail.com>
2024-12-18 20:23:12 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 147ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241218/정수 사각형 최댓값의 최소/minimax-path-in-square.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static int[][] grid;
+    public static int[][] dp;
+
+    public static void find() {
+        dp[0][0] = grid[0][0];
+
+        for(int i = 1; i < n; i ++) {
+            dp[0][i] = Math.max(dp[0][i - 1], grid[0][i]);
+            dp[i][0] = Math.max(dp[i - 1][0], grid[i][0]);
+        }
+
+        for(int i = 1; i < n; i ++) {
+            for(int j = 1; j < n; j ++) {
+                dp[i][j] = Math.max(Math.min(dp[i - 1][j], dp[i][j - 1]), grid[i][j]);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        grid = new int[n][n];
+        dp = new int[n][n];
+
+        for(int i = 0; i < n; i ++) {
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j ++) {
+                grid[i][j] = Integer.parseInt(stk.nextToken());
+            }
+        }
+
+        find();
+
+        bw.write(dp[n - 1][n - 1] + "");
+        bw.flush();
+    }
+}
