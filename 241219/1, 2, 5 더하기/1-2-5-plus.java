c09200b5d71dfa49a8e9d4a718544c229961dfa7
Siu Hwang <siuh0403@gmail.com>
2024-12-19 22:13:17 +0900

    Title: 1, 2, 5 더하기, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

--- 241219/1, 2, 5 더하기/1-2-5-plus.java
+++ 241219/1, 2, 5 더하기/1-2-5-plus.java
@@ -11,18 +11,14 @@ public class Main {
 
         n = Integer.parseInt(br.readLine());
 
-        dp = new int[n + 6];
+        dp = new int[n + 1];
 
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
-        dp[4] = 5;
-        dp[5] = 9;
+        dp[0] = 1;
 
-        for(int i = 6; i <= n; i ++) {
+        for(int i = 1; i <= n; i ++) {
             for(int j = 0; j < 3; j ++) {
                 if(i >= arr[j]) {
-                    dp[i] += dp[i - arr[j]];
+                    dp[i] = (dp[i] + dp[i - arr[j]]) % 10007;
                 }
             }
         }
