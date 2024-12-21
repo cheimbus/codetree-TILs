55a24f11158268c11b1395f02d38d40297184c64
Siu Hwang <siuh0403@gmail.com>
2024-12-21 17:07:39 +0900

    Title: 최대 증가 부분 수열, Time: 126ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241221/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static int[] arr;
+    public static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        dp = new int[n + 1];
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i ++) {
+            arr[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        for(int i = 1; i <= n; i ++) {
+            for(int j = i - 1; j >= 0; j --) {
+                if(i >= j) {
+                    if(arr[i] > arr[j]) {
+                        dp[i] = Math.max(dp[j] + 1, dp[i]);
+                    }
+                }
+            }
+        }
+
+        int ans = 0;
+        for(int i = 1; i <= n; i ++) {
+            ans = Math.max(ans, dp[i]);
+        }
+
+        bw.write(ans + "");
+        bw.flush();
+    }
+}
