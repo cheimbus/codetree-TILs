a6b3d2f5874f0423ee65f3a65c0be22559f7ccca
Siu Hwang <siuh0403@gmail.com>
2025-01-08 14:49:26 +0900

    Title: 아름다운 수, Time: 103ms, Memory: 9MB, Status: Passed - CodeTree

--- 250108/아름다운 수/beautiful-number.java
+++ 250108/아름다운 수/beautiful-number.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static boolean find() {
         for(int i = 0; i < arr.size(); i += arr.get(i)) {
-            if(i + arr.get(i) >= n) return false;
+            if(i + arr.get(i) - 1 >= n) return false;
             for(int j = i; j < i + arr.get(i); j ++) {
                 if(arr.get(i) != arr.get(j)) return false;
             }
