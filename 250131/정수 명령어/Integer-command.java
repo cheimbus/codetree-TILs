490f08ebb392aa2e75a2fedb3deca1d19d343b99
Siu Hwang <siuh0403@gmail.com>
2025-01-31 13:12:25 +0900

    Title: 정수 명령어, Time: 503ms, Memory: 23MB, Status: Passed - CodeTree

--- 250131/정수 명령어/Integer-command.java
+++ 250131/정수 명령어/Integer-command.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.Map.Entry;
 import java.io.*;
 
 public class Main {
@@ -7,32 +6,32 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        TreeSet<Integer> ts = new TreeSet<>();
+        int n = Integer.parseInt(br.readLine());  // 테스트 케이스 개수
         StringBuilder sb = new StringBuilder();
 
-        while(n -- > 0) {
-            int a = Integer.parseInt(br.readLine());
-            while(a -- > 0) {
+        while (n-- > 0) {
+            int a = Integer.parseInt(br.readLine());  // 연산 개수
+            TreeSet<Integer> ts = new TreeSet<>();  // 매 테스트 케이스마다 초기화
+
+            while (a-- > 0) {
                 String[] sArr = br.readLine().split(" ");
-                if(sArr[0].equals("I")) {
+                String command = sArr[0];
+
+                if (command.equals("I")) {  // 삽입
                     ts.add(Integer.parseInt(sArr[1]));
-                }
-                else if(sArr[0].equals("D") && sArr[1].equals("1")) {
-                    if(ts.isEmpty()) {
-                        continue;
+                } else if (!ts.isEmpty()) {  // 삭제 연산은 비어있을 때 실행 안 함
+                    if (command.equals("D")) {
+                        if (sArr[1].equals("1")) ts.pollLast();   // 최댓값 삭제
+                        else if (sArr[1].equals("-1")) ts.pollFirst();  // 최솟값 삭제
                     }
-                    else ts.remove(ts.last());
-                }
-                else if(sArr[0].equals("D") && sArr[1].equals("-1")) {
-                    if(ts.isEmpty()) {
-                        continue;
-                    }
-                    else ts.remove(ts.first());
                 }
             }
-            if(ts.isEmpty()) sb.append("EMPTY").append("\n");
-            else sb.append(ts.last() + " ").append(ts.first()).append("\n");
+
+            if (ts.isEmpty()) {
+                sb.append("EMPTY\n");
+            } else {
+                sb.append(ts.last()).append(" ").append(ts.first()).append("\n");
+            }
         }
 
         bw.write(sb.toString());
