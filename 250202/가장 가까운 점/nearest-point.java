f8f381d1cb2ccb5e7b25e6e9918c57bfac43b48f
Siu Hwang <siuh0403@gmail.com>
2025-02-02 16:15:54 +0900

    Title: 가장 가까운 점, Time: 407ms, Memory: 23MB, Status: Passed - CodeTree

--- 250202/가장 가까운 점/nearest-point.java
+++ 250202/가장 가까운 점/nearest-point.java
@@ -2,18 +2,17 @@ import java.util.*;
 import java.io.*;
 
 class Tuple implements Comparable<Tuple> {
-    int x;
-    int y;
+    int x, y;
+
     public Tuple(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
     @Override
     public int compareTo(Tuple t) {
-        if(x + y != t.x + t.y) {
-            return (x + y) - (t.x + t.y);
-        }
-        if(x != t.x) return x - t.x;
+        if (x + y != t.x + t.y) return (x + y) - (t.x + t.y);
+        if (x != t.x) return x - t.x;
         return y - t.y;
     }
 }
@@ -28,26 +27,26 @@ public class Main {
         int m = Integer.parseInt(stk.nextToken());
 
         PriorityQueue<Tuple> pq = new PriorityQueue<>();
-        while(n -- > 0) {
+
+        while (n-- > 0) {
             stk = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(stk.nextToken());
             int b = Integer.parseInt(stk.nextToken());
             pq.add(new Tuple(a, b));
-
-            Tuple t = pq.poll();
-            int tx = t.x + 2;
-            int ty = t.y + 2;
-
-            pq.add(new Tuple(tx, ty));
         }
 
-        StringBuilder sb = new StringBuilder();
-        Tuple at = pq.peek();
-        int ax = at.x;
-        int ay = at.y;
+        while (m-- > 0 && !pq.isEmpty()) {
+            Tuple t = pq.poll();
+            pq.add(new Tuple(t.x + 2, t.y + 2));
+        }
 
-        sb.append(ax + " ").append(ay);
-        bw.write(sb.toString());
+        if (pq.isEmpty()) {
+            bw.write("0 0\n");
+        } else {
+            Tuple at = pq.peek();
+            bw.write(at.x + " " + at.y + "\n");
+        }
+
         bw.flush();
     }
 }
